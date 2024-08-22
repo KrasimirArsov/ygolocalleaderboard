96dc2ca46398dc286216b81457e28977fa9e46bc
Krasimir Arsov <you@example.com>
2024-08-22 19:55:08 +0300

    Consuming SetCard web service

---
+++ src/main/java/com/k/arsov/ygolocalleaderboard/Main.java
@@ -0,0 +1,11 @@
+package com.k.arsov.ygolocalleaderboard;
+
+import com.k.arsov.ygolocalleaderboard.entity.Duel;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+
+    }
+}
